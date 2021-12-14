e441480d507160c5862590fe3695f20313d43bb6
l33th <l33th@protonmail.ch>
2021-12-14 14:24:52 +0200

    left paddle creation

--- py-pong.py
+++ py-pong.py
@@ -8,3 +8,12 @@ window.title("Pong Game - by WMouton | l33th")
 window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
+
+# create left paddle
+leftpaddle = t.Turtle()
+leftpaddle.speed(0)
+leftpaddle.shape("square")
+leftpaddle.color("white")
+leftpaddle.shapesize(stretch_wid=5, stretch_len=1)
+leftpaddle.penup()
+leftpaddle.goto(-350, 0)
